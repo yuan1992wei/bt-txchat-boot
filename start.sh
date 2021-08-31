0973fe6746a77802e4269cc2606debecaf937b0e
BatainWord <850882387@qq.com>
2021-08-31 04:19:16 +0000

    update start.sh.

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 rm -f tpid
-nohup /home/jdk/jdk1.8.0_201/bin/java -Dfile.encoding=utf-8 -jar bt-txchat-boot-1.0.0.jar | /usr/local/sbin/cronolog ./logs/console-%Y-%m-%d.log >> /dev/null 2>&1 &
+nohup /home/jdk/jdk1.8.0_201/bin/java -Xms256m -Xmx1024m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m -Dfile.encoding=utf-8 -jar bt-txchat-boot-1.0.0.jar | /usr/local/sbin/cronolog ./logs/console-%Y-%m-%d.log >> /dev/null 2>&1 &
 echo $! > tpid
 echo Start Success!
